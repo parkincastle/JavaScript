11b1893a381da023802e67b9196d95015b02aa7c
박인성 <shfoemesmsrpwjd1@gmail.com>
2021-06-21 11:57:10 +0000

    dataType

---
+++ basic-js/Operator/ternaryOperator.js
@@ -0,0 +1,4 @@
+var a = 2, b = 1;
+
+(a > b)? (console.log("a is bigger than b")):(console.log("a is smaller than b"));
+// a is bigger than b
---
+++ basic-js/dataType/boolean.js
@@ -0,0 +1,14 @@
+var result1 = new Boolean();
+var result2 = true;
+
+typeof result1;
+// "object"
+typeof result1.valueOf();
+// "boolean"
+
+console.log(Boolean("test"));
+// true
+console.log(Boolean(""));
+// false
+console.log(Boolean({}));
+// true
